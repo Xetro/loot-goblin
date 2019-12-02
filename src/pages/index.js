ed5b77b623fe33f49ed8ed9e8f728c2c6c388260
Xetro <golob.matevz@gmail.com>
2019-12-02 02:46:19 +0100

    Add suppressors data

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,10 @@ import Header from "../components/header"
 import TierGrid from "../components/tierGrid"
 import * as moment from 'moment';
 
-import JSONData from "../../content/loot-data-20191201195500.json"
+import lootData from "../../content/loot-data-20191201195500.json"
+import suppressorData from "../../content/suppressors-data-20191201195545.json"
+
+const JSONData = lootData.concat(suppressorData);
 
 const mapped = JSONData.map(item => {
   let sum = item.price_array.reduce((acc, val) => acc + parseInt(val), 0);
