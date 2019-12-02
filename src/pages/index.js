24b252f33f5bc68619b3c4955dd6bafd8014e921
Xetro <golob.matevz@gmail.com>
2019-12-02 02:59:28 +0100

    Change styling for last price update

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export default () => (
   <div>
     <div className='alpha'>Alpha version 0.0.1 test</div>
     <Header />
-    <div className='updated'>Last update: {timeFromUpdated}</div>
+    <div className='updated'>Last market price update: <span className='updated-price'>{timeFromUpdated}</span></div>
     <TierGrid itemData={mapped}/>
   </div>
 )
