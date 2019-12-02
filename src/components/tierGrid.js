9bb74f1d9bb7fccf9eadec4de31a2bbad62b6806
Xetro <golob.matevz@gmail.com>
2019-12-02 01:45:47 +0100

    Testing changes on prod

--- src/components/tierGrid.js
+++ src/components/tierGrid.js
@@ -12,8 +12,8 @@ export default props => (
     </div>
 
     {props.itemData.map((item, index) => {
-      return <div className={[styles.row, styles.flexTable].join(' ')}>
-        <div className={styles.flexCell}><img src={item.imagePath} key={item.imagePath}></img></div>
+      return <div className={[styles.row, styles.flexTable].join(' ')} key={`content_item_${item.name}`}>
+        <div className={styles.flexCell}><img src={item.imagePath} key={item.imagePath} alt={item.name}></img></div>
         <div className={styles.flexCell}><span><strong>{item.name}</strong></span></div>
         <div className={styles.flexCell}><span>{item.slots}</span></div>
         <div className={styles.flexCell}><span>{item.avgPrice.toLocaleString()} &#8381;</span></div>
