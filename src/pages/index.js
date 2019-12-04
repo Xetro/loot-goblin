d1d08cb3e8fbff86befb1b055e343c27e81bf436
Xetro <golob.matevz@gmail.com>
2019-12-05 00:38:57 +0100

    Add site metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from "../components/header"
 import TierGrid from "../components/tierGrid"
+import { Helmet } from "react-helmet"
 import * as moment from 'moment';
 
 let updated = '20191205001200';
@@ -8,7 +9,12 @@ let timeFromUpdated = moment(updated, 'YYYYMMDDHHmmss').from(moment());
 
 export default () => (
   <div>
-    <div className='alpha'>Alpha version 0.0.1 test</div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>EFT Loot Goblin</title>
+      <link rel="canonical" href="https://eft-loot.com/" />
+    </Helmet>
+    <div className='alpha'>Alpha version 0.0.2 test</div>
     <Header />
     <div className='updated'>Last market price update: <span className='updated-price'>{timeFromUpdated}</span></div>
     <TierGrid />
