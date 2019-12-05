7ba456b98569c7cfc665724e7261e84150f0d4b2
Xetro <golob.matevz@gmail.com>
2019-12-05 01:59:21 +0100

    Add basic search

--- src/components/tierGrid.js
+++ src/components/tierGrid.js
@@ -1,58 +1,48 @@
 import React from "react"
 import styles from "./tierGrid.module.scss"
-import { StaticQuery, graphql } from "gatsby"
+import * as moment from 'moment';
 
-const TierGrid = ({ data }) => (
-  <StaticQuery
-    query={graphql`
-      {
-        allDataJson(sort: {order: DESC, fields: price_per_slot}) {
-          nodes {
-            name
-            price_array
-            size {
-              height
-              width
-            }
-            imagePath
-            category
-            id
-          }
-        }
-      }
-    `}
-    render={data => 
+let updated = '20191205001200';
+let timeFromUpdated = moment(updated, 'YYYYMMDDHHmmss').from(moment());
+
+export default class TierGrid extends React.Component {
+  state = {
+    data: this.props.graphData.allDataJson.nodes,
+    filteredData: this.props.graphData.allDataJson.nodes,
+  }
+  handleInputChange = event => {
+    const value = event.target.value;
+    let data = this.state.data.filter(item => item.name.toLowerCase().includes(value.toLowerCase()));
+    this.setState({
+      filteredData: data,
+    })
+  }
+  render() {
+    return (
       <div className={styles.container}>
-      <div className={[styles.header, styles.flexTable].join(' ')}>
-        <div className={styles.flexCell}>Image</div>
-        <div className={styles.flexCell}>Name</div>
-        <div className={styles.flexCell}>Slots</div>
-        <div className={styles.flexCell}>Average Price</div>
-        <div className={styles.flexCell}>Price per slot</div>
-      </div>
-      {data.allDataJson.nodes.map((item) => {
-        let avgPrice;
-          if (item.price_array.length > 3) {
-            let sliced = item.price_array.slice(0, 3);
-            let sum = sliced.reduce((acc, val) => acc + parseInt(val), 0);
-            avgPrice = Math.floor(sum / sliced.length);
-          } else {
-            let sum = item.price_array.reduce((acc, val) => acc + parseInt(val), 0);
-            avgPrice = Math.floor(sum / item.price_array.length);
-          }
-          let slots = item.size.width * item.size.height;
-          let pricePerSlot = Math.floor(avgPrice / slots);
-        return <div className={[styles.row, styles.flexTable].join(' ')} key={`content_wrap_${item.id}`} >
-          <div className={styles.flexCell}><img src={item.imagePath} key={`content_image_${item.id}`} alt={item.name}></img></div>
-          <div className={styles.flexCell} key={`content_name_${item.id}`}><span><strong>{item.name}</strong></span></div>
-          <div className={styles.flexCell} key={`content_slots_${item.id}`}><span>{slots}</span></div>
-          <div className={styles.flexCell} key={`content_avgPrice_${item.id}`}><span>{avgPrice.toLocaleString()} &#8381;</span></div>
-          <div className={styles.flexCell} key={`content_pps_${item.id}`}><span>{pricePerSlot.toLocaleString()} &#8381;</span></div>
+        <div className={styles.topRow}>
+          <div className="left">Search: <input className={styles.searchBar} type="search" name="formsearch" onChange={this.handleInputChange}/></div>
+          <div className='updated'>Last market price update: <span className='updated-price'>{timeFromUpdated}</span></div>
         </div>
-      })}
-  
-    </div>
-    }
-  ></StaticQuery>
-)
-export default TierGrid
+        <div className={styles.tableContainer}>
+          <div className={[styles.header, styles.flexTable].join(' ')}>
+          <div className={styles.flexCell}>Image</div>
+          <div className={styles.flexCell}>Name</div>
+          <div className={styles.flexCell}>Slots</div>
+          <div className={styles.flexCell}>Average Price</div>
+          <div className={styles.flexCell}>Price per slot</div>
+        </div>
+        {this.state.filteredData.map((item) => {
+          return <div className={[styles.row, styles.flexTable].join(' ')} key={`content_wrap_${item.id}`} >
+            <div className={styles.flexCell}><img src={item.imagePath} key={`content_image_${item.id}`} alt={item.name}></img></div>
+            <div className={styles.flexCell} key={`content_name_${item.id}`}><span><strong>{item.name}</strong></span></div>
+            <div className={styles.flexCell} key={`content_slots_${item.id}`}><span>{item.slots}</span></div>
+            <div className={styles.flexCell} key={`content_price_avg_${item.id}`}><span>{item.price_avg.toLocaleString()} &#8381;</span></div>
+            <div className={styles.flexCell} key={`content_price_per_slot_${item.id}`}><span>{item.price_per_slot.toLocaleString()} &#8381;</span></div>
+          </div>
+        })}
+        </div>
+      </div>
+    )
+  }
+} 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,39 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Header from "../components/header"
 import TierGrid from "../components/tierGrid"
 import { Helmet } from "react-helmet"
-import * as moment from 'moment';
 
-let updated = '20191205001200';
-let timeFromUpdated = moment(updated, 'YYYYMMDDHHmmss').from(moment());
 
-export default () => (
+
+
+export default ({ data }) => (
   <div>
     <Helmet>
       <meta charSet="utf-8" />
       <title>EFT Loot Goblin</title>
       <link rel="canonical" href="https://eft-loot.com/" />
     </Helmet>
-    <div className='alpha'>Alpha version 0.0.2 test</div>
+    <div className='alpha'>Alpha version 0.0.3 test</div>
     <Header />
-    <div className='updated'>Last market price update: <span className='updated-price'>{timeFromUpdated}</span></div>
-    <TierGrid />
+    <TierGrid graphData={ data }/>
   </div>
 )
+
+
+export const query = graphql`
+  {
+    __typename
+    allDataJson(sort: {order: DESC, fields: price_per_slot}) {
+      nodes {
+        price_avg
+        price_array
+        name
+        price_per_slot
+        slots
+        imagePath
+        id
+      }
+    }
+  }
+`
