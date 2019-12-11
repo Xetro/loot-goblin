6c88317df0f89679d82cc9c023e35873d6951f12
Xetro <golob.matevz@gmail.com>
2019-12-12 00:10:10 +0100

    Change footer hidden time to 1 day

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import moment from "moment";
 
 const hideFooter = ($event) => {
   $event.target.parentElement.classList.toggle(styles.hidden);
-  window.localStorage.setItem('hideFooter', moment().add(2, 'd').format());
+  window.localStorage.setItem('hideFooter', moment().add(1, 'd').format());
 }
 
 export default () => (
