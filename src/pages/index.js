e38c013f63df3bfefe2bdd936a13f34f5b34cc2a
Xetro <golob.matevz@gmail.com>
2019-12-11 16:31:24 +0100

    Add FB and twitter meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,32 @@ import Footer from "../components/footer"
 import TierGrid from "../components/tierGrid"
 import { Helmet } from "react-helmet"
 import moment from "moment";
-import goblinImage from '../../static/loot-goblin.png' 
 
 export default ({ data }) => (
   <div>
     <Helmet>
       <html lang='en' />
       <meta charSet="utf-8" />
-      <title>EFT Loot Goblin</title>
-      <meta name="description" content="Browse and compare Escape from Tarkov Flea Market item prices." />
+      <title>EFT Loot Goblin - Flea Market Companion</title>
+      <meta name="title" content="EFT Loot Goblin - Flea Market Companion" />
+      <meta name="description" content="Browse Escape from Tarkov Flea Market item prices.  Compare thousands items based on their market price or price per slot. Including weapons, gear, keys, loot, mods, provisions and more" />
+
       <meta name="keywords" content="EFT,Escape from Tarkov,loot goblin,EFT loot,eft-loot,tarkov loot goblin,tarkov flea market, flea market prices" /> 
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="canonical" href="https://www.eft-loot.com" />
-      <meta property="og:image" content={goblinImage} />
+
+      <meta property="og:type" content="website"></meta>
+      <meta property="og:url" content="https://metatags.io/" />
+      <meta property="og:title" content="EFT Loot Goblin - Flea Market Companion" />
+      <meta property="og:description" content="Browse Escape from Tarkov Flea Market item prices.  Compare thousands items based on their market price or price per slot. Including weapons, gear, keys, loot, mods, provisions and more" />
+      <meta property="og:image" content="https://www.eft-loot.com/loot-goblin.png" />
+
+      <meta property="twitter:card" content="summary_large_image"></meta>
+      <meta property="twitter:url" content="https://metatags.io/" />
+      <meta property="twitter:title" content="EFT Loot Goblin - Flea Market Companion" />
+      <meta property="twitter:description" content="Browse Escape from Tarkov Flea Market item prices.  Compare thousands items based on their market price or price per slot. Including weapons, gear, keys, loot, mods, provisions and more" />
+      <meta property="twitter:image" content="https://www.eft-loot.com/loot-goblin.png" />
+      
     </Helmet>
     <div className='version'>
       <span>Beta version 0.2.5</span>
