c8eea44d2106f5085fa0a56427792d15fdb49b06
Xetro <golob.matevz@gmail.com>
2019-12-19 23:20:21 +0100

    Remove PayPal from footer and test BMC snippet injection

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,12 +14,12 @@ export default () => (
     </div>
     <span className={styles.middle}>Item information and images are provided by <a href="https://escapefromtarkov.gamepedia.com/Escape_from_Tarkov_Wiki">The Official Escape from Tarkov Wiki</a> under <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/legalcode">CC BY-NC-SA 3.0</a> license</span>
     <div className={styles.support}>
-      <span>Support the project: </span>
+      {/* <span>Support the project: </span>
       <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
         <input type="hidden" name="cmd" value="_s-xclick" />
         <input type="hidden" name="hosted_button_id" value="UDWAZ49TVGT48" />
         <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-      </form>
+      </form> */}
     </div>
   </footer>
 )
